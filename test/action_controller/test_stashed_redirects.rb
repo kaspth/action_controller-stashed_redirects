14d0a77a890f8c7e92d52ea2012cbfd911433a6c
Chris Oliver <excid3@gmail.com>
2022-04-21 10:46:08 -0500

    Remove unused test

--- test/action_controller/test_stashed_redirects.rb
+++ test/action_controller/test_stashed_redirects.rb
@@ -6,8 +6,4 @@ class ActionController::TestStashedRedirects < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::ActionController::StashedRedirects::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
