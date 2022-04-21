d26cc131cebd52f258c1980a2048948927593b04
Kasper Timm Hansen <kaspth@gmail.com>
2022-04-21 21:24:17 +0200

    Cave in and include rails/test_help plus cut requires

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,14 +3,9 @@
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 
 require "debug"
-
-require "action_controller"
-require "action_controller/stashed_redirects"
-
-require "action_dispatch/testing/integration"
-
 require "rails"
-require "rails/application"
+require "rails/test_help"
+require "action_controller/stashed_redirects"
 require "minitest/autorun"
 
 require_relative "boot/action_controller"
@@ -29,11 +24,4 @@ Rails.application.initialize!
 
 Rails.application.routes.draw do
   resources :sessions, :users
-end
-
-class ActionDispatch::IntegrationTest
-  def before_setup
-    @routes = Rails.application.routes
-    super
-  end
 end
