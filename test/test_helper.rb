7fb59c906c473238747fd2516d4e2aefd86fe9c5
Kasper Timm Hansen <kaspth@gmail.com>
2022-04-21 12:28:09 +0200

    Start setting up tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+
+require "action_controller"
 require "action_controller/stashed_redirects"
+
+require "action_dispatch/testing/integration"
 
 require "minitest/autorun"
