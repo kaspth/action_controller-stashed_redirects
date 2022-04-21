927d608abbfdee049bc973b167d4d12df9767374
Kasper Timm Hansen <kaspth@gmail.com>
2022-04-21 11:33:53 +0200

    scaffold: bundle gem action_controller-stashed_redirects

---
+++ lib/action_controller/stashed_redirects.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "stashed_redirects/version"
+
+module ActionController
+  module StashedRedirects
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "action_controller/stashed_redirects"
+
+require "minitest/autorun"
