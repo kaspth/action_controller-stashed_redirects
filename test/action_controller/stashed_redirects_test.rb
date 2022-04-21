303710fb514184b76afed7dbab1a0fc9e7c6397b
Kasper Timm Hansen <kaspth@gmail.com>
2022-04-21 16:26:00 +0200

    Ok fine setup a teeny Rails app for testing

--- test/action_controller/stashed_redirects_test.rb
+++ test/action_controller/stashed_redirects_test.rb
@@ -3,4 +3,19 @@
 require "test_helper"
 
 class ActionController::StashedRedirectsTest < ActionDispatch::IntegrationTest
+  test "stash and recall redirect from a param" do
+    get new_session_path, params: { redirect_url: users_path }
+    assert_response :no_content
+
+    post sessions_path
+    assert_redirected_to users_path
+  end
+
+  test "stash and recall redirect from the referer" do
+    get new_session_path, headers: { HTTP_REFERER: users_path }
+    assert_response :no_content
+
+    post sessions_path
+    assert_redirected_to users_path
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,6 +7,18 @@ require "action_controller/stashed_redirects"
 
 require "action_dispatch/testing/integration"
 
+require "rails"
+require "rails/application"
 require "minitest/autorun"
 
 require_relative "boot/action_controller"
+
+class ActionController::StashedRedirects::Application < Rails::Application
+  config.eager_load = false
+end
+
+Rails.application.initialize!
+
+Rails.application.routes.draw do
+  resources :sessions, :users
+end
